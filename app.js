94dd593b282b8c82291afd02c5850ad25f74fd21
Lee So Young <cheerup0101@naver.com>
2019-10-18 16:40:25 +0900

    mode

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d');
 const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 
 canvas.width = 700;
@@ -10,17 +12,18 @@ ctx.strokeStyle ="#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
   painting = false;
-}
+};
 
 function startPainting() {
   painting = true;
 
-}
+};
 
-function MouseMove(event) {
+function mouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
   if(!painting) {
@@ -30,28 +33,54 @@ function MouseMove(event) {
      ctx.lineTo(x,y);
      ctx.stroke();
   }
-}
+};
 
 function colorClick(event) {
   const bgColor = event.target.style.backgroundColor; //내가 클릭한 그것의 CSS스타일
   ctx.strokeStyle = bgColor;
-}
+};
+
+function rangeChange(evnet) {
+  const rangeValue = event.target.value;
+  ctx.lineWidth = rangeValue;
+};
+
+function modeClick() {
+   if(filling === true) {
+     filling = false;
+     mode.innerText = 'FILL'
+   } else {
+     filling = true;
+     mode.innerText = 'paint'
+   }
+
+
+
+  // if(mode.innerHTML === 'FILL') {
+  //    mode.innerHTML = 'paint';
+  // } else {
+  //   mode.innerHTML = 'FILL'
+  // }
+};
 
 if(canvas) {
-  canvas.addEventListener('mousemove',MouseMove);
+  canvas.addEventListener('mousemove',mouseMove);
   canvas.addEventListener('mousedown',startPainting);  //mousedown:클릭시 발생하는 이벤트
   canvas.addEventListener('mouseup',stopPainting);
   canvas.addEventListener('mouseleave',stopPainting);
-}
+};
 
 Array.from(colors).forEach(color =>
   color.addEventListener('click',colorClick));
 
 
+if(range) {
+  range.addEventListener('input',rangeChange);
+};
 
-
-
-
+if(mode) {
+  mode.addEventListener('click',modeClick)
+};
 
 
 
